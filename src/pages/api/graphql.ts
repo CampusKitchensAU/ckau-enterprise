07c94b80fccafc146ac049746ae5e2901dc99269
roberte777 <robewilkes@yahoo.com>
2022-12-06 19:28:10 -0600

    using apollo now

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,21 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { createYoga } from "graphql-yoga";
+import { ApolloServer } from "@apollo/server";
 import { schema } from "../../graphql/schema";
-import type { NextApiRequest, NextApiResponse } from "next";
+import { startServerAndCreateNextHandler } from "../../graphql/handler";
 
 // Docs: https://vercel.com/docs/concepts/functions/serverless-functions
+const server = new ApolloServer({ schema, introspection: true });
 
-export const config = {
-  api: {
-    // Disable body parsing (required for file uploads)
-    bodyParser: false,
-  },
-};
+export default startServerAndCreateNextHandler(server);
 
-export default createYoga<{
-  req: NextApiRequest;
-  res: NextApiResponse;
-}>({
-  graphqlEndpoint: "/api/graphql",
-  schema: schema,
-});
+//Yoga example in case apollo gets on my nerves
+//this is the only thing needed for a base setup with yoga
+// export const config = {
+//   api: {
+//     // Disable body parsing (required for file uploads)
+//     bodyParser: false,
+//   },
+// };
+// export default createYoga<{
+//   req: NextApiRequest;
+//   res: NextApiResponse;
+// }>({
+//   graphqlEndpoint: "/api/graphql",
+//   schema: schema,
+// });
