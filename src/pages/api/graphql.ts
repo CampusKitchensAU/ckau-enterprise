50361913addf282c5082f27306dd9c3d87a9ca86
roberte777 <robewilkes@yahoo.com>
2022-12-07 16:00:32 -0600

    testing gql

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,12 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { ApolloServer } from "@apollo/server";
-import { schema } from "../../graphql/schema";
-import { startServerAndCreateNextHandler } from "@as-integrations/next";
+// import { ApolloServer } from "@apollo/server";
+// import { schema } from "../../graphql/schema";
+// import { startServerAndCreateNextHandler } from "@as-integrations/next";
 
 // Docs: https://vercel.com/docs/concepts/functions/serverless-functions
-const server = new ApolloServer({ schema });
+// const server = new ApolloServer({ schema });
 
-export default startServerAndCreateNextHandler(server);
+// export default startServerAndCreateNextHandler(server);
 
 //Yoga example in case apollo gets on my nerves
 //this is the only thing needed for a base setup with yoga
@@ -23,3 +23,45 @@ export default startServerAndCreateNextHandler(server);
 //   graphqlEndpoint: "/api/graphql",
 //   schema: schema,
 // });
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+
+const typeDefs = /* GraphQL */ `
+  type Query {
+    users: [User!]!
+    user(username: String): User
+    greetings: String
+  }
+  type User {
+    name: String
+    username: String
+  }
+`;
+
+const users = [
+  { name: "Leeroy Jenkins", username: "leeroy" },
+  { name: "Foo Bar", username: "foobar" },
+];
+
+const resolvers = {
+  Query: {
+    users() {
+      return users;
+    },
+    user(_parent: any, { username }: { username: string }) {
+      return users.find((user) => user.username === username);
+    },
+    greetings() {
+      return "Hello World!";
+    },
+  },
+};
+
+export const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+const server = new ApolloServer({
+  schema,
+});
+
+export default startServerAndCreateNextHandler(server);
