da1cba0e94ac856d5cb507ed8664147bf554546b
roberte777 <robewilkes@yahoo.com>
2022-12-07 16:11:42 -0600

    trying new loadFilesSync values

--- src/graphql/schema/index.ts
+++ src/graphql/schema/index.ts
@@ -2,10 +2,13 @@ import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeTypeDefs } from "@graphql-tools/merge";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import resolvers from "./resolvers";
+import path from "path";
 
 export const schema = makeExecutableSchema({
   typeDefs: mergeTypeDefs(
-    loadFilesSync("src/graphql/schema/**/*.graphql", { recursive: true })
+    loadFilesSync(path.join(process.cwd(), "./**/*.graphql"), {
+      recursive: true,
+    })
   ),
   resolvers,
 });
