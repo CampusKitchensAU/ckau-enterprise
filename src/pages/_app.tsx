3d4cf5b0ce2613fb84f323e6a193c79036e031a3
roberte777 <robewilkes@yahoo.com>
2023-01-14 19:46:03 -0600

    added trpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,19 @@
 import { type AppType } from "next/app";
+import { trpc } from "../utils/trpc";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 import "../styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
+    Component,
+    pageProps: { session, ...pageProps },
 }) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+    return (
+        <SessionProvider session={session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
 };
 
-export default MyApp;
+export default trpc.withTRPC(MyApp);
