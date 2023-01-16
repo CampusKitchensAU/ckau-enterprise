4aac35e1381156f1b0d60a3f5169220189997564
roberte777 <roberte777@users.noreply.github.com>
2023-01-16 06:11:06 +0000

    Apply formatting changes

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,7 +1,7 @@
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import type { AppRouter } from "../server/routers/_app";
-let module = 'test';
+let module = "test";
 function getBaseUrl() {
   if (typeof window !== "undefined")
     // browser should use relative path
