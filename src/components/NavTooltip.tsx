bab03b57430f73dd953ceff0493fd8dbc85568de
Trevor Aupperle <trevora@hightouchinc.com>
2023-01-16 23:19:56 -0600

    Quick change on tooltip.

--- src/components/NavTooltip.tsx
+++ src/components/NavTooltip.tsx
@@ -3,7 +3,7 @@ const NavTooltip = ({ title }: { title: string }) => {
       <div
         id="tooltip"
         role="tooltip"
-        className="absolute top-1 -right-5 hidden translate-x-full 
+        className="absolute top-1 -right-6 hidden translate-x-full 
             rounded-lg bg-gray-700 px-2 py-1 text-center text-sm text-white
             before:absolute before:top-1/2 before:right-[100%] 
             before:-translate-y-1/2  before:border-8 before:border-y-transparent 
