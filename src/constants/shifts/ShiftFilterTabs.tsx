c397c33852e43c747762e5613fa0852794d8e6d3
TrevorAupperle <tza0038@auburn.edu>
2023-01-23 16:41:58 -0600

    Added shift list filters.

---
+++ src/constants/shifts/ShiftFilterTabs.tsx
@@ -0,0 +1,36 @@
+/** Need this file because I need different filter functions for shifts than
+I do with contacts. */
+interface FilterTab {
+    title: string;
+    searchFn: () => void;
+  }
+  
+  const ShiftFilterTabs: FilterTab[] = [
+    {
+      title: "All",
+      searchFn: () => {
+        return;
+      },
+    },
+    {
+      title: "Pickup",
+      searchFn: () => {
+        return;
+      },
+    },
+    {
+      title: "Packaging",
+      searchFn: () => {
+        return;
+      },
+    },
+    {
+      title: "Delivery",
+      searchFn: () => {
+        return;
+      },
+    },
+  ];
+  
+  export default ShiftFilterTabs;
+  
