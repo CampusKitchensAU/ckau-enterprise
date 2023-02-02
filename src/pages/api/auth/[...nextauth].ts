8028abf0972539dfe8babdfc6e8f0a786fba7961
roberte777 <robewilkes@yahoo.com>
2023-02-01 18:21:11 -0600

    made it so that profile info updates if it updates from the used provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -9,20 +9,67 @@ import { prisma } from "../../../server/db";
 
 export const authOptions: NextAuthOptions = {
   callbacks: {
-    async signIn({ user, profile }) {
-      console.log(profile);
+    async signIn({ user, profile, account }) {
       //update user data to match changes in profile
       if (user.id && profile) {
-        if (user.name !== profile.name || user.image !== profile.image) {
-          // await prisma.user.update({
-          //   where: {
-          //     id: user.id,
-          //   },
-          //   data: {
-          //     name: profile?.name,
-          //     image: profile?.image,
-          //   },
-          // });
+        const databaseUser = await prisma.user.findUnique({
+          where: {
+            id: user.id,
+          },
+        });
+        if (databaseUser) {
+          switch (account?.provider) {
+            case "google":
+              if (
+                profile.name != databaseUser.name ||
+                profile.picture != databaseUser.image ||
+                profile.email != databaseUser.email
+              ) {
+                await prisma.user.update({
+                  where: {
+                    id: user.id,
+                  },
+                  data: {
+                    name: profile.name,
+                    email: profile.email,
+                    image: profile.picture,
+                  },
+                });
+              }
+              break;
+            case "azure-ad":
+              const profilePhotoSize = 48;
+              const profilePicture = await fetch(
+                `https://graph.microsoft.com/v1.0/me/photos/${profilePhotoSize}x${profilePhotoSize}/$value`,
+                {
+                  headers: {
+                    Authorization: `Bearer ${account.access_token}`,
+                  },
+                }
+              );
+              const pictureBuffer = await profilePicture.arrayBuffer();
+              const pictureBase64 =
+                Buffer.from(pictureBuffer).toString("base64");
+              const pictureData = `data:image/png;base64,${pictureBase64}`;
+
+              if (
+                profile.name != databaseUser.name ||
+                pictureData != databaseUser.image ||
+                profile.email != databaseUser.email
+              ) {
+                await prisma.user.update({
+                  where: {
+                    id: user.id,
+                  },
+                  data: {
+                    name: profile.name,
+                    email: profile.email,
+                    image: pictureData,
+                  },
+                });
+              }
+              break;
+          }
         }
       }
       return true;
@@ -51,8 +98,8 @@ export const authOptions: NextAuthOptions = {
       },
     }),
     AzureADProvider({
-      clientId: process.env.AZURE_AD_CLIENT_ID,
-      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+      clientId: process.env.AZURE_AD_CLIENT_ID!,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
       tenantId: process.env.AZURE_AD_TENANT_ID,
       // authorization: { params: { scope: "openid profile user.Read email" } },
     }),
